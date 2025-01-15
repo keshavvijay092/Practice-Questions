99d1f762250abb2ba264896bf751c29d5bb73903
keshavvijay092 <keshavvijay092@gmail.com>
2025-01-15 22:02:45 +0530

    this is the link of practice questions of 15th

---
+++ 13th Jan 2025/217.ContainsDuplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int>mp;
+
+        for(auto i:nums)
+        {
+            mp[i]++;
+            if(mp[i] >=2) return true;
+        }
+        return false;
+    }
+};
